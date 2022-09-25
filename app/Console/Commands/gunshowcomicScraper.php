dfafab0874bd1a8d9a212515b1c1e2e92a75da3d
Meeli <meeli.sirvi@tptlive.ee>
2022-09-25 18:03:11 +0300

    added gunshowcomic scraper

--- app/Console/Commands/gunshowcomicScraper.php
+++ app/Console/Commands/gunshowcomicScraper.php
@@ -6,14 +6,14 @@ use GuzzleHttp\Client;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Cache;
 
-class XkcdScraper extends Command
+class gunshowcomicScraper extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'scraper:xkcd';
+    protected $signature = 'scrape:gunshow';
 
     /**
      * The console command description.
@@ -30,23 +30,20 @@ class XkcdScraper extends Command
     public function handle()
     {
         $client = new Client(['verify' => false]);
-        $bar = $this->output->createProgressBar(100);
-        $bar->start();
-        for($i=1;$i<100;$i++) {
-            $bar->advance();
-            if(Cache::has('xkcd-'. $i)){
-                $html = Cache::get('xkcd-'. $i);
+        for($i=1;$i<10;$i++) {
+
+            if(Cache::has('gunshow-'. $i)){
+                $html = Cache::get('gunshow-'. $i);
             } else {
-                $response = $client->get('https://xkcd.com/' . $i);
+                $response = $client->get('https://gunshowcomic.com/' . $i);
                 $html = $response->getBody()->getContents();
-                Cache::put('xkcd-'. $i, $html);
+                Cache::put('gunshow-'. $i, $html);
             }
             $crawler = new \Symfony\Component\DomCrawler\Crawler($html);
-            $imgEl = $crawler->filter('#comic img');
-//            var_dump($imgEl->attr('src'));
-//            var_dump($imgEl->attr('title'));
-//            var_dump($imgEl->attr('alt'));
+            $imgEl = $crawler->filter('.strip');
+            var_dump($imgEl->attr('src'));
+            var_dump($imgEl->attr('title'));
+
         }
-        $bar->finish();
     }
 }
